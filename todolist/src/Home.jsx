2cbc01d5c752722e91516454422f1bdbc94aa223
Nhat Xuan <nhatxuaan@gmail.com>
2026-04-24 23:17:39 +0700

    Init: Home screen

---
+++ todolist/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Create from '../Create'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Create/>
+    </div>
+  )
+}
+
+export default Home
