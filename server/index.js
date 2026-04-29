c6985be236ce5ba1685a4f08e117eece5fa83bc8
Nhat Xuan <nhatxuaan@gmail.com>
2026-04-29 16:20:33 +0700

    update: delete task (be)

--- server/index.js
+++ server/index.js
@@ -9,6 +9,12 @@ app.use(express.json())
 
 mongoose.connect('mongodb://127.0.0.1:27017/test')
 
+app.get('/get',(req,res)=>{
+    TodoModel.find()
+    .then(result => res.json(result))
+    .catch (err => res.json(err))
+})
+
 app.post('/add',(req,res)=>{
     const task = req.body.task;
     TodoModel.create({
@@ -16,6 +22,22 @@ app.post('/add',(req,res)=>{
     }).then(result => res.json(result))
     .catch(err =>res.json(err))
 })
+
+app.put('/update/:id',(req,res)=>{
+    const {id} = req.params;
+    // console.log(id);
+    TodoModel.findByIdAndUpdate({_id:id},{done:true})
+    .then(result => res.json(result))
+    .catch(err => err.json(err))
+})
+
+app.delete('/delete/:id', (req,res)=>{
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id:id})
+    .then(result=>res.json(result))
+    .catch(err=>err.json(err))
+})
+
 app.listen(3001,()=>{
     console.log("Server is running...")
 })
